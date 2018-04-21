6d243553a767bda4a5b8407bfbcde3df4b14842c
SURAJ <suraj@getvsm.com>
2018-04-21 16:36:31 +0530

    assignment 3 git page

---
+++ js/controllers/github-app-ctrl.js
@@ -0,0 +1,39 @@
+gitHubApp.controller('gitHubAppCtrl', function ($scope, githubRepoData) {
+
+    $scope.submit = function () {
+        //code
+    }
+
+    $scope.cancel = function () {
+        $('.textarea-div').css('display', 'none');
+        $('.add-bio').show();
+    }
+
+    $scope.addBio = function () {
+        $('.add-bio').hide();
+        $('.textarea-div').css('display', 'block');
+
+    }
+
+    $scope.getData = function () {
+        githubRepoData.githubData().then(function (data) {
+            $scope.githubData = data;
+        });
+    }
+
+    $scope.getData();
+
+    $(window).scroll(function () {
+        if (window.scrollY > 100) {
+            $('.main-content-nav-bar').css('z-index', '111').css('position', 'fixed').css('top', '0');
+        } else {
+            $('.main-content-nav-bar').css('position', 'static')
+        }
+        if (window.scrollY > 350) {
+            $('.scroll-bar').css('display', 'block').css('background', 'white');
+        } else {
+            $('.scroll-bar').css('display', 'none')
+        }
+    })
+
+})
---
+++ js/services/github-app-data-service.js
@@ -0,0 +1,16 @@
+gitHubApp.service('githubRepoData', function ($http, $q) {
+    this.githubData = function () {
+        var deferred = $q.defer();
+        var url = 'json/'
+        var fileName = 'repo-data.json'
+        $http({
+            method: 'GET',
+            url: url + fileName,
+        }).success(function (data) {
+            deferred.resolve(data);
+        }).error(function (data, status) {
+            deferred.reject(status)
+        });
+        return deferred.promise;
+    }
+})
